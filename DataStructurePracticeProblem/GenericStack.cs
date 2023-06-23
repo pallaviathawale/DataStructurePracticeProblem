f251af312d4d64c18f5cb030a16f85698be22bd1
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 17:46:07 +0530

    [PALLAVI] Add: Generic Using Stack

---
+++ DataStructurePracticeProblem/GenericStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePracticeProblem
+{
+    public class GenericsStack<T>
+    {
+        private Node<T> top;
+
+        public GenericsStack()
+        {
+            top = null;
+        }
+
+        private void IfTopisNull()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty!!!");
+            }
+        }
+
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        public void Display()
+        {
+            IfTopisNull();
+            Node<T> temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine("" + temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- DataStructurePracticeProblem/Program.cs
+++ DataStructurePracticeProblem/Program.cs
@@ -5,15 +5,22 @@
         static void Main(string[] args)
         {
            
-            Console.WriteLine("Welcome to Generics Data Structures!!!\n");
+            //Console.WriteLine("Welcome to Generics Data Structures!!!\n");
 
-            LinkedList<int> list = new LinkedList<int>();
+            //LinkedList<int> list = new LinkedList<int>();
 
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
 
-            list.Display();
+            //list.Display();
+
+
+            GenericsStack<int> stack = new GenericsStack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
     
