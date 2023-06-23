1c398fddf6a1cca3cde7a4fb9b47e6f14a6f8f3e
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 17:40:47 +0530

    [PALLAVI] Add: File

---
+++ DataStructurePracticeProblem/GenericLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePracticeProblem
+{
+    public class LinkedList<T>
+        {
+            private Node<T> head;
+
+            public void Add(T data)
+            {
+                Node<T> newNode = new Node<T>(data);
+
+                if (head == null)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    Node<T> current = head;
+
+                    while (current.next != null)
+                    {
+                        current = current.next;
+                    }
+
+                    current.next = newNode;
+                }
+            }
+
+            public void Display()
+            {
+                Node<T> temp = head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked list is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.WriteLine(""+ temp.data +"");
+                    temp = temp.next;
+                }
+            }
+        }
+
+
+    
+}
---
+++ DataStructurePracticeProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DataStructurePracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Welcome to Generics Data Structures!!!\n");
+
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
+        }
+    }
+    
+    
+}
